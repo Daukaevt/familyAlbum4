0b499138492e6159ae07bb4ccb0cc0f23969ec9b
Daukaevt <78514903+Daukaevt@users.noreply.github.com>
2024-12-26 22:10:48 +0300

    init commit

---
+++ familyAlbum3-4/src/test/java/com/wixsite/mupbam1/FamilyAlbum34ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wixsite.mupbam1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyAlbum34ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
