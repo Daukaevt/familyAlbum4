37cdddfe9a2b8874e4afb61d90d9061f5fe97aa8
Daukaevt <78514903+Daukaevt@users.noreply.github.com>
2025-01-05 14:53:06 +0300

    PhotoController added; SecurityConfig added

---
+++ familyAlbum3-4/src/main/java/com/wixsite/mupbam1/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.wixsite.mupbam1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.*;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth->{
+			auth.requestMatchers("/").permitAll();
+			auth.anyRequest().authenticated();
+		})
+		.formLogin(withDefaults())
+		.oauth2Login(withDefaults());
+		return http.build();		
+	} 
+
+}
